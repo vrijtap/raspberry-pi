566a2faa3646affcccba2bb0a17337d685bab16e
bergb15 <brianvdberg2107@gmail.com>
2023-11-23 17:05:32 +0100

    Classify method and comments

--- cupClassificationLib/cupClassification.py
+++ cupClassificationLib/cupClassification.py
@@ -7,18 +7,53 @@ class CupClassification:
     Initializes an instance of CupClassification with a given model.
 
     Parameters:
-    - model (str): A string representing the file path to the model.
+    - model_path (str): A string representing the file path to the model.
+    - classification_threshold (float): A float representing the classification threshold. Default is 0.5.
     """
-    def __init__(self, model_path: str) -> None:
-        model = load_model(model_path)
-        # Display the loaded model summary
-        model.summary()
+    def __init__(self, model_path: str, classification_threshold: float = 0.5) -> None:
+        """
+        Initialize the CupClassification instance.
 
-        X = np.array([cv2.imread('test-image.jpg', cv2.IMREAD_GRAYSCALE)])
+        Parameters:
+        - model_path (str): A string representing the file path to the model.
+        - classification_threshold (float): A float representing the classification threshold. Default is 0.5.
+        """
+        # Use the path to load the model
+        try:
+            self.model = load_model(model_path)
+            print("Model loaded successfully.")
+        except Exception as e:
+            print(f"Error loading the model: {e}")
+        
+        # Set the (default) classification threshold
+        self.classification_threshold = classification_threshold
+
+    def classify(self, img: np.ndarray) -> bool:
+        """
+        Classify the input image and determine if it contains a cup.
+
+        Parameters:
+        - img (numpy.ndarray): Input image as a NumPy array with shape (64, 64, 3).
+
+        Returns:
+        - bool: True if the model predicts the presence of a cup with confidence above the threshold, False otherwise.
+
+        Raises:
+        - ValueError: If the input image shape is not (64, 64, 3).
+        """
+        if img.shape != (64, 64, 3):
+            raise ValueError("Input image shape must be (64, 64, 3)")
+
+        # Transform the data for model input
+        X = np.array([img])
         X = X / 255.0
 
-        predictions = model.predict(X)
-        print(predictions)
+        # Return True if the model predicts the presence of a cup with confidence above the threshold
+        predictions = self.model.predict(X)
+        return True if predictions[0] > self.classification_threshold else False
 
+# test main
 if __name__ == '__main__':
     instance = CupClassification('model.h5')
+    result = instance.classify(cv2.imread('test-image.jpg', cv2.IMREAD_COLOR))
+    print(result)
