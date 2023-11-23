93a1cb05e7b25a4ef8a149fe6f1598f89b1c3f20
bergb15 <brianvdberg2107@gmail.com>
2023-11-23 17:09:05 +0100

    Added libraries to __init__

--- cupClassificationLib/__init.py
+++ cupClassificationLib/__init.py
@@ -1,4 +1,4 @@
 # cupClassificationLib/__init__.py
-
-# tensorflow import (compatible with python 3.9, 3.10 or 3.11)
-
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
