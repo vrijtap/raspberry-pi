f99567c15fd82a306d8c4376251d3203477cc4a9
Diego-Brandjes <diegobrandjes@hotmail.com>
2023-11-30 15:24:53 +0100

    prints out weight data for checking

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ This file is the main program loop for a larger project that controls the Raspbe
 It saves multiple pictures in a folder or path specified by the user of the function and interacts with an RFID reader.
 
 Author: Diego Brandjes
-Date:   06-10-2023
+Date:   30-11-2023
 """
 from cameraLib  import camera
 from mongoLib  import mongo
 from decouple import config
 from rfidLib import readerRFID
 from i2cLib import bus
-    
+
 SM_PAUSED_STATE  = 2
 SM_START = 1
 SM_STOP = 0
@@ -55,7 +55,9 @@ def main():
                 # This statement will read the weight data only when the read data is within the desired range.
                 if i2c.receive_data() > 2 and i2c.receive_data() < 255:
                     weightData = i2c.receive_data()
+                    print(weightData) # use for checking
                     weightLoop = False
+                    break
 
         if input("Quit? (y/n): ").strip().lower() == 'y':
             break
